65a9dad9f7640d879b104855f8c2ed6e5682f47b
Constantin Sandmann <constantin@cksandman.de>
2019-04-06 23:38:48 +0200

    Updated config.ini

--- Resources/generated_config.cpp
+++ Resources/generated_config.cpp
@@ -1,4 +1,4 @@
 #include "generated_config.h"
 
-const char* pcConfig_content = "[SandBox]\nhostname = SandBox\nport = 8080\ncacheDirectory = cache\nresourceDirectory = ../Resources\n\n[Logging]\nloglevel = DEBUG\nlogfile = sand.log\n\n[Spotify]\nclientId = xxx\nclientSecret = xxx\ndevice = SandBox\n\n[Reader]\nreadIntervalInMs = 500\n";
-const unsigned int nConfig_size = 256;
+const char* pcConfig_content = "[SandBox]\nhostname = SandBox\nport = 8080\ncacheDirectory = cache\nresourceDirectory = ../Resources\n\n[Logging]\nloglevel = DEBUG\nlogfilePath = cache/sand.log\n\n[Spotify]\nclientId = xxx\nclientSecret = xxx\ndevice = SandBox\n\n[Reader]\nreadIntervalInMs = 500\n";
+const unsigned int nConfig_size = 266;
